a0b51d0b144bfd0cd68f218a92520f690aad5313
Fahad Mohammad <fahad.mohammad06@gmail.com>
2022-06-14 02:41:46 -0400

    Completed JS callbacks, promises, Async/Await

--- callback.js
+++ callback.js
@@ -10,14 +10,15 @@ function getPosts() {
         output += `<li>${post.title}</li>`;
         });
         document.body.innerHTML = output;
-    }, 1000)
+    }, 1000);
 }
 
-function createPost(post) {
+function createPost(post, callback) {
     setTimeout(() => {
         posts.push(post);
+        callback();
     }, 2000);
 }
-getPosts();
 
-createPost({ title: 'Post Three', body: 'This is post three' });
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
